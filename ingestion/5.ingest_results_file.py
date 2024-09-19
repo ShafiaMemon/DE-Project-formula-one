0abe8a4cebf628573a8137246314fa333ed60f76
Shafia Memon <133508187+ShafiaMemon@users.noreply.github.com>
2024-09-19 21:17:36 +0500

    Add files via upload

---
+++ ingestion/5.ingest_results_file.py
@@ -0,0 +1,133 @@
+# Databricks notebook source
+# MAGIC %md
+# MAGIC ### Ingest results.json file
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 1 - Read the JSON file using the spark dataframe reader API
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_data_source", "")
+v_data_source = dbutils.widgets.get("p_data_source")
+
+# COMMAND ----------
+
+dbutils.widgets.text("p_file_date", "2021-03-28")
+v_file_date = dbutils.widgets.get("p_file_date")
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/configuration"
+
+# COMMAND ----------
+
+# MAGIC %run "../includes/common_functions"
+
+# COMMAND ----------
+
+from pyspark.sql.types import StructType, StructField, IntegerType, StringType, FloatType
+
+# COMMAND ----------
+
+results_schema = StructType(fields=[StructField("resultId", IntegerType(), False),
+                                    StructField("raceId", IntegerType(), True),
+                                    StructField("driverId", IntegerType(), True),
+                                    StructField("constructorId", IntegerType(), True),
+                                    StructField("number", IntegerType(), True),
+                                    StructField("grid", IntegerType(), True),
+                                    StructField("position", IntegerType(), True),
+                                    StructField("positionText", StringType(), True),
+                                    StructField("positionOrder", IntegerType(), True),
+                                    StructField("points", FloatType(), True),
+                                    StructField("laps", IntegerType(), True),
+                                    StructField("time", StringType(), True),
+                                    StructField("milliseconds", IntegerType(), True),
+                                    StructField("fastestLap", IntegerType(), True),
+                                    StructField("rank", IntegerType(), True),
+                                    StructField("fastestLapTime", StringType(), True),
+                                    StructField("fastestLapSpeed", FloatType(), True),
+                                    StructField("statusId", StringType(), True)])
+
+# COMMAND ----------
+
+results_df = spark.read \
+.schema(results_schema) \
+.json(f"{raw_folder_path}/{v_file_date}/results.json")
+
+# COMMAND ----------
+
+display(results_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 2 - Rename columns and add new columns
+
+# COMMAND ----------
+
+from pyspark.sql.functions import lit
+
+# COMMAND ----------
+
+from pyspark.sql.functions import current_timestamp
+
+# COMMAND ----------
+
+results_with_columns_df = results_df.withColumnRenamed("resultId", "result_id") \
+                                    .withColumnRenamed("raceId", "race_id") \
+                                    .withColumnRenamed("driverId", "driver_id") \
+                                    .withColumnRenamed("constructorId", "constructor_id") \
+                                    .withColumnRenamed("positionText", "position_text") \
+                                    .withColumnRenamed("positionOrder", "position_order") \
+                                    .withColumnRenamed("fastestLap", "fastest_lap") \
+                                    .withColumnRenamed("fastestLapTime", "fastest_lap_time") \
+                                    .withColumnRenamed("fastestLapSpeed", "fastest_lap_speed") \
+                                    .withColumn("ingestion_date", current_timestamp()) \
+                                    .withColumn("data_source", lit(v_data_source)) \
+                                    .withColumn("file_date", lit(v_file_date))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 3 - Drop the unwanted column
+
+# COMMAND ----------
+
+from pyspark.sql.functions import col
+
+# COMMAND ----------
+
+results_final_df = results_with_columns_df.drop(col("statusId"))
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##### Step 4 - Write to output to processed container in parquet format
+
+# COMMAND ----------
+
+#for race_id_list in results_final_df.select("race_id").distinct().collect():
+    #if (spark._jsparkSession.catalog().tableExists("f1_processed.results")):
+     #spark.sql(f"ALTER TABLE f1_processed.results DROP IF EXISTS PARTITION (race_id = {race_id_list.race_id})")
+
+# COMMAND ----------
+
+#results_final_df.write.mode("append").partitionBy('race_id').format("parquet").saveAsTable("f1_processed.results")
+
+# COMMAND ----------
+
+overwrite_partition(results_final_df, 'f1_processed', 'results', 'race_id')
+
+# COMMAND ----------
+
+# MAGIC %sql
+# MAGIC SELECT race_id, COUNT(1) 
+# MAGIC FROM f1_processed.results
+# MAGIC GROUP BY race_id
+# MAGIC ORDER BY race_id DESC;
+
+# COMMAND ----------
+
+dbutils.notebook.exit("Success")
