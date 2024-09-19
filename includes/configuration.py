da0608273a67327e0847b4e100b3f7ed45dac63e
Shafia Memon <133508187+ShafiaMemon@users.noreply.github.com>
2024-09-19 21:19:45 +0500

    Add files via upload

---
+++ includes/configuration.py
@@ -0,0 +1,4 @@
+# Databricks notebook source
+raw_folder_path = "/mnt/datalformulaone/raw"
+processed_folder_path = "/mnt/datalformulaone/processed"
+presentation_folder_path = "/mnt/datalformulaone/presentation"
